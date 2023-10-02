4d9d33f705405945052519c937016429e044fd5d
Alicja <39903590+AlaZet@users.noreply.github.com>
2023-10-02 21:16:35 +0200

    Update readme and improve the quiz

--- ai_quiz/src/App.js
+++ ai_quiz/src/App.js
@@ -1,73 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import questions from './questions.json';
 import './App.css';
 
 function App() {
-  const questions = [
-    {
-      questionText: 'Jakie jest pytanie?',
-      answerOptions: [
-        { answerText: 'Odpowiedź 1', isCorrect: false },
-        { answerText: 'Odpowiedź 2', isCorrect: false },
-        { answerText: 'Odpowiedź 3', isCorrect: false },
-        { answerText: 'Odpowiedź 4', isCorrect: false },
-      ],
-    },
-  ];
+  const [quizQuestions, setQuestions] = useState([]);
+  const [selectedAnswers, setSelectedAnswers] = useState([]); // Dodaj zmienną selectedAnswers
+  const [answersChecked, setAnswersChecked] = useState(false);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
 
-  const currentQuestion = 0; // Możesz zmieniać numer pytania w zależności od tego, które pytanie chcesz wyświetlić
+  useEffect(() => {
+    setQuestions(questions);
+  }, []);
+
+  const currentQuestion = 0;
 
   const handleAnswerClick = (index) => {
-    // Tworzymy kopię tablicy selectedAnswers
     const updatedSelectedAnswers = [...selectedAnswers];
-  
-    // Sprawdzamy, czy odpowiedź jest już zaznaczona
     if (updatedSelectedAnswers.includes(index)) {
-      // Jeśli jest zaznaczona, usuwamy ją ze stanu
       updatedSelectedAnswers.splice(updatedSelectedAnswers.indexOf(index), 1);
     } else {
-      // Jeśli nie jest zaznaczona, dodajemy ją do stanu
       updatedSelectedAnswers.push(index);
     }
-  
-    // Aktualizujemy stan selectedAnswers
     setSelectedAnswers(updatedSelectedAnswers);
   };
 
-  const [selectedAnswers, setSelectedAnswers] = useState([]);
+  const handleCheckAnswer = () => {
+    const correctAnswersIndexes = quizQuestions[currentQuestion].answerOptions
+      .filter((answerOption) => answerOption.isCorrect)
+      .map((answerOption, index) => index);
 
+    setCorrectAnswers(correctAnswersIndexes);
+    setAnswersChecked(true);
+  };
 
   return (
     <div className="quiz-container">
       <div className="header">Certified Tester AI Testing (CT-AI)</div>
-      <div className="quiz-title"style={{ marginBottom: '60px' }}>QUIZ</div>
+      <div className="quiz-title" style={{ marginBottom: '60px' }}>
+        QUIZ
+      </div>
       <div className="question-container">
-        <div className="question" style={{ marginTop: '20px', marginBottom: '40px' }}>Pytanie: {questions[currentQuestion].questionText}</div>
+        <div
+          className="question"
+          style={{ marginTop: '20px', marginBottom: '40px' }}
+        >
+          Question: {quizQuestions[currentQuestion]?.questionText}
+        </div>
 
         <ul className="answer-list">
-        {questions[currentQuestion].answerOptions.map((answerOption, index) => (
-          <li className="answer-list-item" key={index}>
-            <label
-              className={`answer-label ${selectedAnswers.includes(index) ? "selected" : ""}`}
-              onClick={() => handleAnswerClick(index)}
-            >
-              <input
-                type="checkbox"
-                name={`answer${index}`}
-                checked={selectedAnswers.includes(index)}
-                onChange={() => { }} // Wyłącz zmiany na checkboxie
-              />
-              <div
-                className={`answer-rectangle ${selectedAnswers.includes(index) ? "selected" : ""}`}
-                onClick={() => handleAnswerClick(index)}
-              >
-                {answerOption.answerText}
-              </div>
-            </label>
-          </li>
-        ))}
-      </ul>
-
+          {quizQuestions[currentQuestion]?.answerOptions.map(
+            (answerOption, index) => (
+              <li className="answer-list-item" key={index}>
+                <label
+                  className={`answer-label ${
+                    selectedAnswers.includes(index) ? 'selected' : ''
+                  }`}
+                  onClick={() => handleAnswerClick(index)}
+                >
+                  <div
+                    className={`answer-rectangle ${
+                      selectedAnswers.includes(index) ? 'selected' : ''
+                    } ${
+                      answersChecked &&
+                      (correctAnswers.includes(index)
+                        ? 'correct'
+                        : !correctAnswers.includes(index)
+                        ? 'incorrect'
+                        : '')
+                    }`}
+                  >
+                    {answerOption.answerText}
+                  </div>
+                </label>
+              </li>
+            )
+          )}
+        </ul>
+        <div
+          className="button-check-answer"
+          style={{ marginTop: '40px' }}
+          onClick={handleCheckAnswer}
+        >
+          Check answer
         </div>
+      </div>
     </div>
   );
 }
